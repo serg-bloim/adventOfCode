fd7d4e5f47939802fba785c5a556546579bd6b42
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2023-12-19 09:22:27 -0500

    Day17 Task 1 is done

--- src/test/kotlin/advent23/Day17.kt
+++ src/test/kotlin/advent23/Day17.kt
@@ -1,7 +1,10 @@
 package advent23
 
+import advent23.Day10.Helper.move
+import advent23.Day10.Helper.withinBox
+import advent23.Day10.Helper.x
+import advent23.Day10.Helper.y
 import org.junit.jupiter.api.Test
-import java.util.Stack
 
 class Day17 {
     internal class Task1 {
@@ -17,57 +20,65 @@ class Day17 {
         fun testReal() {
             val actual = Solution.solve(load_prod())
             println("Result: $actual")
-            assertEquals(12345, actual)
+            assertEquals(936, actual)
         }
 
     }
 
-    data class Args(val x: Int, val y: Int, val straight: Int, val dir: Direction, val cost: Int)
     object Solution {
+        var minPath = 0
         fun solve(txt: String): Any {
+            minPath = Int.MAX_VALUE
             val city = txt.lines().map { it.map { it.digitToInt() } }
-            val xMax = city.first().indices.last
-            val yMax = city.indices.last
-            val cache = Array(3) { Array(city.size) { IntArray(city.first().size) { Int.MAX_VALUE } } }
-            val stack = Stack<Args>()
-            stack.push(Args(0, 0, 0, Direction.East, 0))
-            while (stack.isNotEmpty()) {
-                search(stack, city, cache)
-            }
-            return cache.minOf { it[yMax][xMax] } - city[0][0]
+            val cache = Array(4) { Array(city.size) { IntArray(city.first().size) { Int.MAX_VALUE } } }
+            start(Direction.East, city, cache)
+            start(Direction.North, city, cache)
+            return minPath
         }
 
-        private fun search(
-            stack: Stack<Args>,
+        private fun start(
+            dir: Direction,
             city: List<List<Int>>,
             cache: Array<Array<IntArray>>
         ) {
-            val (x, y, straight, dir, cost) = stack.pop()
-            if (y !in city.indices || x !in city.first().indices) return
-            val selfCost = city[y][x]
-            val cacheCost = cache[straight][y][x]
-            val currentCost = cost + selfCost
-            if (currentCost >= cacheCost) return
-            if (currentCost >= cache.last().last().last()) return
-            for (s in straight..2){
-                cache[s][y][x] = currentCost
-            }
-            dir.right().let { dir ->
-                val (xNew, yNew) = move(x, y, dir)
-                stack.push(Args(xNew, yNew, 0, dir, currentCost))
-            }
-            dir.left().let { dir ->
-                val (xNew, yNew) = move(x, y, dir)
-                stack.push(Args(xNew, yNew, 0, dir, currentCost))
-            }
-            if (straight < 2) {
-                val (xNew, yNew) = move(x, y, dir)
-                stack.push(Args(xNew, yNew, straight + 1, dir, currentCost))
-            }
+            val startingPos = Coords(0, 0).move(dir)
+            search(startingPos, dir, 0, city, cache)
         }
 
-        fun traverse(x: Int, y: Int, straight: Int) {
+        private fun search(
+            coords: Coords,
+            dir: Direction,
+            cost: Int,
+            city: List<List<Int>>,
+            cache: Array<Array<IntArray>>
+        ) {
+            val (x, y) = coords
+            if (y !in city.indices || x !in city.first().indices) return
+            var currentCost = cost
+            val cacheCost = cache[dir.ordinal][y][x]
+            if (currentCost >= cacheCost) return
+            cache[dir.ordinal][y][x] = currentCost
+            var midBLock = coords
+            for (fwd in 0..2) {
+                currentCost += city[midBLock.y][midBLock.x]
+                if (currentCost >= minPath) return
+                if (midBLock.y == city.lastIndex && midBLock.x == city.first().lastIndex) {
+                    if (currentCost < minPath) {
+                        minPath = currentCost
+                    }
+                }
 
+                dir.left().let { dir ->
+                    search(midBLock.move(dir), dir, currentCost, city, cache)
+                }
+                dir.right().let { dir ->
+                    search(midBLock.move(dir), dir, currentCost, city, cache)
+                }
+                midBLock = midBLock.move(dir)
+                if (!midBLock.withinBox(city.first().size, city.size)) {
+                    break
+                }
+            }
         }
     }
 
