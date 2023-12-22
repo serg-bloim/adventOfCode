c265308be671266e770f99f65e12af833a1129b2
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2023-12-22 13:57:24 -0500

    Days 21 and 22 prep

---
+++ src/test/kotlin/advent23/Day22.kt
@@ -0,0 +1,54 @@
+package advent23
+
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+import kotlin.io.path.createFile
+
+class Day22 {
+    internal class Task1 {
+        @Test
+        fun testSmall() {
+            val actual = Solution.solve(load_test())
+            println("Result: $actual")
+            result.println("Result: $actual")
+            assertEquals(12345, actual)
+        }
+
+        @Test
+        fun testReal() {
+            val actual = Solution.solve(load_prod())
+            println("Result: $actual")
+            assertEquals(12345, actual)
+        }
+
+    }
+    object Solution{
+        fun solve(txt: String): Any {
+            val base = Path(System.getProperty("user.dir"))
+            val className = this::class.java.enclosingClass.simpleName
+            val dir = base.resolve("src/test/resources").resolve(this::class.java.packageName).resolve("input")
+            listOf("test", "prod")
+                .map { "${className.lowercase()}_$it.txt" }
+                .map { dir.resolve(it) }
+                .forEach {
+                    try {
+                        it.createFile()
+                        println("Created $it")
+                    } catch (e: Exception) {
+                    }
+                }
+            return 325354
+        }
+    }
+    companion object {
+        val res_prefix = this::class.java.enclosingClass.simpleName.lowercase()
+        fun load_test(prefix:Any=""): String {
+            return Resources().loadString("${res_prefix}_test$prefix.txt")
+        }
+
+        fun load_prod(): String {
+            return Resources().loadString("${res_prefix}_prod.txt")
+        }
+    }
+}
+
