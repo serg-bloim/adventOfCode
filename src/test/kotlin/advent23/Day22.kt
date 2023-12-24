1f615c800931205608d2a243613cc36486c58122
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2023-12-23 21:22:51 -0500

    Day 22 Task2 is done

--- src/test/kotlin/advent23/Day22.kt
+++ src/test/kotlin/advent23/Day22.kt
@@ -23,6 +23,24 @@ class Day22 {
 
     }
 
+    internal class Task2 {
+        @Test
+        fun testSmall() {
+            val actual = Solution.solve2(load_test())
+            println("Result: $actual")
+            result.println("Result: $actual")
+            assertEquals(7, actual)
+        }
+
+        @Test
+        fun testReal() {
+            val actual = Solution.solve2(load_prod())
+            println("Result: $actual")
+            assertEquals(71002, actual)
+        }
+
+    }
+
     object Solution {
         val brickRe = Regex("""(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)""")
         private fun parseBrick(str: String): Brick {
@@ -37,12 +55,19 @@ class Day22 {
         }
 
         fun solve(txt: String): Any {
+            val bricks = parseInput(txt)
+            val bricksWithSingleSupport =
+                bricks.asSequence().map { it.supports }.filter { it.size == 1 }.flatten().distinct().count()
+            return bricks.size - bricksWithSingleSupport
+        }
+
+        private fun parseInput(txt: String): List<Brick> {
             val bricks = txt.lineSequence()
                 .map { parseBrick(it) }
                 .sortedBy { it.zmin }
                 .toList()
             val openBricks = mutableSetOf<Brick>()
-            val supports = bricks.map { b ->
+            bricks.forEach { b ->
                 val intersects = openBricks.asSequence().filter { b.intersectXY(it) }.toList()
                 val topZ = intersects.maxOfOrNull { it.zmax } ?: 0
                 val supports = intersects.filter { it.zmax == topZ }
@@ -50,14 +75,32 @@ class Day22 {
                 b.zmin -= dz
                 b.zmax -= dz
                 openBricks.add(b)
-                supports
+                b.supports = supports
             }
-            val bricksWithSingleSupport = supports.asSequence().filter { it.size == 1 }.flatten().distinct().count()
-            return bricks.size - bricksWithSingleSupport
+            return bricks
+        }
+
+        fun solve2(txt: String): Any {
+            val bricks = parseInput(txt).sortedBy { it.zmax }
+            return bricks.sumOf { b ->
+                val fallenBricks = mutableSetOf(b)
+                var fallenCounter = 0
+                while (fallenBricks.isNotEmpty()) {
+                    val zmax = fallenBricks.minOf { it.zmax }
+                    val batch = fallenBricks.filter { it.zmax == zmax }.toSet()
+                    fallenBricks.removeAll(batch)
+                    val newFallen = bricks.filter { it.zmin == zmax + 1 && batch.containsAll(it.supports) }
+                    fallenCounter += newFallen.size
+                    fallenBricks.addAll(newFallen)
+                }
+                fallenCounter
+            }
         }
     }
 
+
     data class Brick(val xmin: Int, val xmax: Int, val ymin: Int, val ymax: Int, var zmin: Int, var zmax: Int) {
+        var supports: List<Brick> = emptyList()
         fun intersectXY(other: Brick): Boolean {
             return IntRange(xmin, xmax).intersectRange(IntRange(other.xmin, other.xmax)).size() > 0 &&
                     IntRange(ymin, ymax).intersectRange(IntRange(other.ymin, other.ymax)).size() > 0
