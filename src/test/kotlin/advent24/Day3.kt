42f67f19fa7de967136a6e635ab325d400d4467e
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2024-12-03 00:12:30 -0500

    Day3 task2 complete. Dirty code

--- src/test/kotlin/advent24/Day3.kt
+++ src/test/kotlin/advent24/Day3.kt
@@ -39,7 +39,7 @@ class Day3 {
             val actual = solve(load_test())
             println("Result: $actual")
             result.println("Result: $actual")
-            assertEquals(4, actual)
+            assertEquals(48, actual)
         }
 
         @Test
@@ -51,8 +51,19 @@ class Day3 {
         }
 
         fun solve(txt: String): Int {
-            val data = parseInput(txt)
-            return 5555
+            val ops = """mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""".toRegex().findAll(txt)
+            var enabled = true
+            var sum = 0
+            for (op in ops) {
+                when (op.value) {
+                    "do()" -> enabled = true
+                    "don't()" -> enabled = false
+                    else -> if (enabled) {
+                        sum += (op.groupValues[1].toInt() * op.groupValues[2].toInt())
+                    }
+                }
+            }
+            return sum
         }
     }
 
