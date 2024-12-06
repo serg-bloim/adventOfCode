27c662535ea1451bca51918bc5bf0436e1c76a73
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2024-12-06 15:38:01 -0500

    Day6 task2 complete
    Did BruteForce as well as fixed the initial solution

--- src/test/kotlin/advent24/Day6.kt
+++ src/test/kotlin/advent24/Day6.kt
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import utils.Coords
 import utils.Direction
+import utils.dbg
 import utils.move
 import utils.result
-import kotlin.io.path.Path
-import kotlin.io.path.createFile
+import utils.withinBox
 import kotlin.test.assertEquals
 
 class Day6 {
@@ -25,7 +25,7 @@ class Day6 {
         fun testReal() {
             val actual = solve(load_prod())
             result.println("Result: $actual")
-            assertEquals(555555555, actual)
+            assertEquals(4515, actual)
             println("Result: $actual")
         }
 
@@ -37,7 +37,7 @@ class Day6 {
             val startPos = 1.let {
                 for (y in field.indices) {
                     for (x in field[y].indices) {
-                        if (field[y][x] == 2) {
+                        if (field[y][x] == Cell.Visited) {
                             return@let Coords(x, y)
                         }
                     }
@@ -45,18 +45,17 @@ class Day6 {
                 return@let Coords(0, 0)
             }
             var pos = startPos
-            var dir = Direction.South
+            var dir = Direction.North
             while (!(pos.x == 0 || pos.y == 0 || pos.x == lastX || pos.y == lastY)) {
                 val next = pos.move(dir)
-                if (field[next.y][next.x] == 1) {
-                    dir = dir.left() // Cause y is inverted
+                if (field[next.y][next.x].canNotVisit) {
+                    dir = dir.right()
                 } else {
                     pos = next
-                    field[next.y][next.x] = 2
-//                    println("Move to ${pos}")
+                    field[next.y][next.x] = Cell.Visited
                 }
             }
-            val visited = field.sumOf { it.count { it == 2 } }
+            val visited = field.sumOf { it.count { it == Cell.Visited } }
             return visited
         }
     }
@@ -68,7 +67,7 @@ class Day6 {
             val actual = solve(load_test())
             println("Result: $actual")
             result.println("Result: $actual")
-            assertEquals(5555555, actual)
+            assertEquals(6, actual)
         }
 
         @Test
@@ -76,12 +75,136 @@ class Day6 {
             val actual = solve(load_prod())
             result.println("Result: $actual")
             println("Result: $actual")
-            assertEquals(55555555, actual)
+            // Accepted result is 1309.
+            // Didn't accept 1354
+            assertEquals(1309, actual)
         }
 
-        fun solve(txt: String): Int {
-            return 11111111
+        @Test
+        fun testRealBruteForce() {
+            val actual = solveBruteForce(load_prod())
+            result.println("Result: $actual")
+            println("Result: $actual")
+            // Accepted result is 1309.
+            // Didn't accept 1354
+            assertEquals(1309, actual)
         }
+
+        private fun solveBruteForce(txt: String): Int {
+            val field = parseInput(txt)
+
+            val startPos = 1.let {
+                for (y in field.indices) {
+                    for (x in field[y].indices) {
+                        if (field[y][x] == Cell.Visited) {
+                            return@let Coords(x, y)
+                        }
+                    }
+                }
+                return@let Coords(0, 0)
+            }
+
+            val potentialObstacles = mutableSetOf<Coords>()
+            for (y in field.indices) {
+                for (x in field[y].indices) {
+                    if (field[y][x].canVisit) {
+                        val newObstacle = Coords(x, y)
+                        if (tryPlaceObstacle(field, startPos, Direction.North, newObstacle)) {
+                            potentialObstacles.add(newObstacle)
+                        }
+                    }
+                }
+            }
+            return potentialObstacles.size
+        }
+
+        fun solve(txt: String): Any {
+            val field = parseInput(txt)
+
+            val startPos = 1.let {
+                for (y in field.indices) {
+                    for (x in field[y].indices) {
+                        if (field[y][x] == Cell.Visited) {
+                            return@let Coords(x, y)
+                        }
+                    }
+                }
+                return@let Coords(0, 0)
+            }
+            val potentialObstacles = mutableSetOf<Coords>()
+            patrol(field, startPos, Direction.North) { coords, dir ->
+                field[coords.y][coords.x] = Cell.Visited
+                val next = coords.move(dir)
+                // Very important to only place a new obstacle to not visited cells.
+                // Cause if there were an obstacle, it couldn't be visited at the first place.
+                // Also, as it is already visited, it has been already tested before.
+                if (field[next.y][next.x] == Cell.Empty) {
+                    if (tryPlaceObstacle(field, coords, dir, next)) {
+                        potentialObstacles.add(next)
+                    }
+                }
+                true
+            }
+
+            return potentialObstacles.asSequence().filter { it.withinBox(field[0].size, field.size) }.count()
+        }
+    }
+
+    private fun tryPlaceObstacle(
+        field: List<MutableList<Day6.Cell>>,
+        guard: Coords,
+        dir: Direction,
+        newObstacle: Coords
+    ): Boolean {
+        field[newObstacle.y][newObstacle.x] = Cell.Obstacle
+        val visited = mutableSetOf<Pair<Coords, Direction>>()
+        var foundLoop = false
+        patrol(field, guard, dir) { pos, dir2 ->
+            if (Pair(pos, dir2) in visited) {
+                foundLoop = true
+                false
+            } else {
+                visited.add(Pair(pos, dir2))
+                true
+            }
+        }
+        field[newObstacle.y][newObstacle.x] = Cell.Empty
+        if (foundLoop) {
+            dbg.println("Obstacle: $newObstacle Loop length: ${visited.size}")
+        }
+        return foundLoop
+    }
+
+    private fun patrol(
+        field: List<MutableList<Cell>>,
+        startPos: Coords,
+        dir: Direction,
+        onStep: (Coords, Direction) -> Boolean
+    ) {
+        val lastX = field[0].size - 1
+        val lastY = field.size - 1
+        var pos = startPos
+        var dir = dir
+        if (pos.x == 0 || pos.y == 0 || pos.x == lastX || pos.y == lastY)
+            return
+        do {
+            if (!onStep(pos, dir)) break
+            val next = pos.move(dir)
+            if (field[next.y][next.x].canNotVisit) {
+                dir = dir.right()
+            } else {
+                pos = next
+            }
+        } while (!(pos.x == 0 || pos.y == 0 || pos.x == lastX || pos.y == lastY))
+    }
+
+    enum class Cell(val canVisit: Boolean) {
+
+        Empty(true),
+        Obstacle(false),
+        Visited(true);
+
+        val canNotVisit = !canVisit
     }
 
     companion object {
@@ -94,17 +217,17 @@ class Day6 {
             return Resources().loadString("${res_prefix}_prod.txt")
         }
 
-        fun parseInput(txt: String): List<MutableList<Int>> {
+        fun parseInput(txt: String): List<MutableList<Cell>> {
             val data = txt.lineSequence().map { line ->
                 line.map {
                     when (it) {
-                        '.' -> 0
-                        '#' -> 1
-                        '^' -> 2
+                        '.' -> Cell.Empty
+                        '#' -> Cell.Obstacle
+                        '^' -> Cell.Visited
                         else -> throw Exception("Bad input")
                     }
                 }.toMutableList()
-            }.toList()
+            }.toList().reversed()
             return data
         }
     }
