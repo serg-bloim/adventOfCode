d29d83a72078a6440fd9fd7ae19bfe0f3f9dec53
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2024-12-09 22:46:00 -0500

    Day9 task1 complete

---
+++ src/test/kotlin/advent24/Day9.kt
@@ -0,0 +1,104 @@
+package advent24
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import utils.result
+import kotlin.test.assertEquals
+
+class Day9 {
+    @Nested
+    inner class Task1 {
+        @Test
+        fun testSmall() {
+            val actual = solve(load_test())
+            assertEquals(1928L, actual)
+            result.println("Result: $actual")
+            println("Result: $actual")
+        }
+
+        @Test
+        fun testReal() {
+            val actual = solve(load_prod())
+            result.println("Result: $actual")
+            assertEquals(6401092019345, actual)
+            println("Result: $actual")
+        }
+
+        fun solve(txt: String): Any {
+            val format1 = parseInput(txt)
+            val files = format1.chunked(2)
+                .mapIndexed { index, pair -> Triple(index, pair.first(), pair.getOrNull(1) ?: 0) }
+                .toList()
+            val blocks = compactIdBlocks(files)
+            return blocks.mapIndexed { i, id -> (i * id).toLong() }.sum()
+        }
+    }
+
+    private fun compactIdBlocks(files: List<Triple<Int, Int, Int>>) = sequence {
+        val fwdItr = files.iterator()
+        val bwdItr = files.asReversed().asSequence()
+            .flatMap { (id, fileSize, freeSpace) -> generateSequence { id }.take(fileSize) }
+            .iterator()
+        var lastBwdFileId = Int.MAX_VALUE
+        while (fwdItr.hasNext()) {
+            val (id, fileSize, freeSpace) = fwdItr.next()
+            if (id < lastBwdFileId) {
+                yieldAll(generateSequence { id }.take(fileSize))
+            } else {
+                if (id == lastBwdFileId) {
+                    yieldAll(bwdItr.asSequence().takeWhile { it == id })
+                }
+                return@sequence
+            }
+            if (freeSpace > 0) {
+                val filesFromEnd = bwdItr.asSequence()
+                    .take(freeSpace) // take maximum blocks as we have free space
+                for (bwdFileId in filesFromEnd) {
+                    // If the bwd file has same or lower ID than fwd file, it means we finished
+                    if (bwdFileId == id) return@sequence
+                    else yield(bwdFileId)
+                    lastBwdFileId = bwdFileId
+                }
+            }
+        }
+    }
+
+    @Nested
+    inner class Task2 {
+        @Test
+        fun testSmall() {
+            val actual = solve(load_test())
+            println("Result: $actual")
+            result.println("Result: $actual")
+            assertEquals(5555555, actual)
+        }
+
+        @Test
+        fun testReal() {
+            val actual = solve(load_prod())
+            result.println("Result: $actual")
+            println("Result: $actual")
+            assertEquals(55555555, actual)
+        }
+
+        fun solve(txt: String): Int {
+            return 11111111
+        }
+    }
+
+    companion object {
+        val res_prefix = this::class.java.enclosingClass.simpleName.lowercase()
+        fun load_test(): String {
+            return Resources().loadString("${res_prefix}_test.txt")
+        }
+
+        fun load_prod(): String {
+            return Resources().loadString("${res_prefix}_prod.txt")
+        }
+
+        fun parseInput(txt: String): Sequence<Int> {
+            val data = txt.asSequence().map { it.digitToInt() }
+            return data
+        }
+    }
+}
