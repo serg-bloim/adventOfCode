19c634233c80a07cc17ab861a95574ba0eaf1f56
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2024-12-11 11:46:32 -0500

    Day11 task2 complete

--- src/test/kotlin/advent24/Day11.kt
+++ src/test/kotlin/advent24/Day11.kt
@@ -2,10 +2,7 @@ package advent24
 
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import utils.dbg
 import utils.result
-import kotlin.io.path.Path
-import kotlin.io.path.createFile
 import kotlin.test.assertEquals
 
 class Day11 {
@@ -13,7 +10,7 @@ class Day11 {
     inner class Task1 {
         @Test
         fun testSmall() {
-            val actual = solve(load_test())
+            val actual = solve(load_test(), 25)
             assertEquals(55312, actual)
             result.println("Result: $actual")
             println("Result: $actual")
@@ -21,55 +18,56 @@ class Day11 {
 
         @Test
         fun testReal() {
-            val actual = solve(load_prod())
+            val actual = solve(load_prod(), 25)
             result.println("Result: $actual")
             assertEquals(197157, actual)
             println("Result: $actual")
         }
-
-        fun solve(txt: String): Any {
-            val stones = parseInput(txt)
-            return stones.map { stone ->
-                countStones(stone, 25)
-            }.sum()
-        }
     }
 
-    private fun countStones(initStone: Long, transforms: Int): Int {
-        if (transforms == 0) {
-            dbg.print("$initStone ")
-            return 1
+    fun solve(txt: String, transforms: Int): Any {
+        val stones = parseInput(txt)
+        val cache = mutableMapOf<Pair<Long, Int>, Long>()
+        return stones.map { stone ->
+            countStones(stone, transforms, cache)
+        }.sum()
+    }
+
+    private fun countStones(initStone: Long, transforms: Int, cache: MutableMap<Pair<Long, Int>, Long>): Long {
+        if (transforms == 0) return 1
+        cache[Pair(initStone, transforms)]?.let { return it }
+        val result = if (initStone == 0L) {
+            countStones(1, transforms - 1, cache)
+        } else {
+            val str = initStone.toString()
+            if (str.length % 2 == 0) {
+                val stone1 = str.subSequence(0, str.length / 2).toString().toLong()
+                val stone2 = str.subSequence(str.length / 2, str.length).toString().toLong()
+                countStones(stone1, transforms - 1, cache) + countStones(stone2, transforms - 1, cache)
+            } else {
+                countStones(initStone * 2024, transforms - 1, cache)
+            }
         }
-        if (initStone == 0L) return countStones(1, transforms - 1)
-        val str = initStone.toString()
-        if (str.length % 2 == 0) {
-            val stone1 = str.subSequence(0, str.length / 2).toString().toLong()
-            val stone2 = str.subSequence(str.length / 2, str.length).toString().toLong()
-            return countStones(stone1, transforms - 1) + countStones(stone2, transforms - 1)
-        }
-        return countStones(initStone * 2024, transforms - 1)
+        cache[Pair(initStone, transforms)] = result
+        return result
     }
 
     @Nested
     inner class Task2 {
         @Test
         fun testSmall() {
-            val actual = solve(load_test())
+            val actual = solve(load_test(), 25)
             println("Result: $actual")
             result.println("Result: $actual")
-            assertEquals(5555555, actual)
+            assertEquals(55312, actual)
         }
 
         @Test
         fun testReal() {
-            val actual = solve(load_prod())
+            val actual = solve(load_prod(), 75)
             result.println("Result: $actual")
             println("Result: $actual")
-            assertEquals(55555555, actual)
-        }
-
-        fun solve(txt: String): Any {
-            return 11111111
+            assertEquals(234430066982597, actual)
         }
     }
 
