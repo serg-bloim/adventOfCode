79f17dcda3156a4482664b9c3b9187a8760ea727
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-12-07 14:22:28 -0500

    Day2 task2 done bruteforce

--- src/test/kotlin/advent25/Day2.kt
+++ src/test/kotlin/advent25/Day2.kt
@@ -14,7 +14,7 @@ class Day2 {
     inner class Task1 {
         @Test
         fun testSmall() {
-            val actual = solve(load_test())
+            val actual = solve(load_test(), ::isValid)
             assertEquals(1227775554, actual)
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
@@ -22,7 +22,7 @@ class Day2 {
 
         @Test
         fun testReal() {
-            val actual = solve(load_prod())
+            val actual = solve(load_prod(), ::isValid)
             result.println("Result: $actual")
             assertEquals(55916882972, actual)
             logger.info { "Result: $actual" }
@@ -34,27 +34,13 @@ class Day2 {
             assertTrue { !isValid(1010) }
         }
 
-        fun solve(txt: String): Any {
-            val data = parseInput(txt)
-            var sum = 0L
-            for (range in data) {
-                val (start, end) = range
-                for (n in start..end) {
-                    if (!isValid(n)) {
-                        sum += n
-                    }
-                }
-            }
-            return sum
-        }
-
         fun isValid(n: Long): Boolean {
             val str = n.toString()
             if (str.length % 2 == 0) {
                 val halfIndex = str.length / 2
                 val first = str.subSequence(0, halfIndex)
                 val second = str.subSequence(halfIndex, str.length)
-                if( first.contentEquals(second)){
+                if (first.contentEquals(second)) {
                     return false
                 }
             }
@@ -62,26 +48,56 @@ class Day2 {
         }
     }
 
+    fun solve(txt: String, isValid: (Long) -> Boolean): Any {
+        val data = parseInput(txt)
+        var sum = 0L
+        for (range in data) {
+            val (start, end) = range
+            for (n in start..end) {
+                if (!isValid(n)) {
+                    sum += n
+                }
+            }
+        }
+        return sum
+    }
+
     @Nested
     inner class Task2 {
         @Test
         fun testSmall() {
-            val actual = solve(load_test())
+            val actual = solve(load_test(), ::isValid)
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
-            assertEquals(5555555, actual)
+            assertEquals(4174379265, actual)
         }
 
         @Test
         fun testReal() {
-            val actual = solve(load_prod())
+            val actual = solve(load_prod(), ::isValid)
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
-            assertEquals(55555555, actual)
+            assertEquals(76169125915, actual)
         }
 
-        fun solve(txt: String): Any {
-            return 11111111
+        @Test
+        fun testIsValid() {
+            assertTrue { isValid(101) }
+            assertTrue { !isValid(1010) }
+            assertTrue { !isValid(111) }
+        }
+
+        fun <T> Sequence<T?>.allEqual(): Boolean = distinct().count() == 1
+
+        fun isValid(n: Long): Boolean {
+            val str = n.toString()
+            for (chunkSize in 1..str.length / 2) {
+                if (str.length % chunkSize == 0) {
+                    if (str.asSequence().chunked(chunkSize).allEqual())
+                        return false
+                }
+            }
+            return true
         }
     }
 
