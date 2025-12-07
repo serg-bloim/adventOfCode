f4116e2638403543424d3ff99eab53f646c4dc2b
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-12-07 17:03:32 -0500

    Day3 task1 fast solution 8m10s

--- src/test/kotlin/advent25/Day3.kt
+++ src/test/kotlin/advent25/Day3.kt
@@ -4,6 +4,7 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import utils.assertEquals
+import utils.pow
 import utils.result
 
 class Day3 {
@@ -33,7 +34,7 @@ class Day3 {
             return data.sumOf { findMaxJoltage(it) }
         }
 
-        private fun findMaxJoltage(batteries: List<Int>): Int {
+        private fun findMaxJoltage(batteries: List<Long>): Long {
             val firstMax = batteries.asSequence()
                 .take(batteries.size - 1)
                 .withIndex()
@@ -53,7 +54,7 @@ class Day3 {
             val actual = solve(load_test())
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
-            assertEquals(5555555, actual)
+            assertEquals(3121910778619, actual)
         }
 
         @Test
@@ -61,11 +62,26 @@ class Day3 {
             val actual = solve(load_prod())
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
-            assertEquals(55555555, actual)
+            assertEquals(172787336861064, actual)
         }
 
         fun solve(txt: String): Any {
-            return 11111111
+            val data = parseInput(txt)
+            return data.map { findMaxJoltage(it, 12) }
+//                .onEach { logger.info { it } }
+                .sum()
+        }
+
+        private fun findMaxJoltage(batteries: List<Long>, n: Int): Long {
+            if (n == 1) {
+                return batteries.max()
+            }
+            val firstMax = batteries.asSequence()
+                .take(batteries.size - n + 1)
+                .withIndex()
+                .maxBy { it.value }
+            val rest = findMaxJoltage(batteries.subList(firstMax.index + 1, batteries.size), n - 1)
+            return firstMax.value * 10L.pow(n - 1) + rest
         }
     }
 
@@ -79,9 +95,9 @@ class Day3 {
             return Resources().loadString("${res_prefix}_prod.txt")
         }
 
-        fun parseInput(txt: String): List<List<Int>> {
+        fun parseInput(txt: String): List<List<Long>> {
             val data = txt.lineSequence().map { line ->
-                line.asSequence().map { it.digitToInt() }.toList()
+                line.asSequence().map { it.digitToInt().toLong() }.toList()
             }.toList()
             return data
         }
