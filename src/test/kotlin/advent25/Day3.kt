4f2930a1587f922be6bcd14488156f3ba58d8cf7
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-12-07 16:47:15 -0500

    Day3 task1 fast solution 6m51s

---
+++ src/test/kotlin/advent25/Day3.kt
@@ -0,0 +1,90 @@
+package advent25
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import utils.assertEquals
+import utils.result
+import kotlin.io.path.Path
+import kotlin.io.path.createFile
+
+class Day3 {
+    val logger = KotlinLogging.logger {}
+
+    @Nested
+    inner class Task1 {
+
+        @Test
+        fun testSmall() {
+            val actual = solve(load_test())
+            assertEquals(357, actual)
+            result.println("Result: $actual")
+            logger.info { "Result: $actual" }
+        }
+
+        @Test
+        fun testReal() {
+            val actual = solve(load_prod())
+            result.println("Result: $actual")
+            assertEquals(555555555, actual)
+            logger.info { "Result: $actual" }
+        }
+
+        fun solve(txt: String): Any {
+            val data = parseInput(txt)
+            val res = data.map { findMaxJoltage(it) }.sum()
+            return res
+        }
+
+        private fun findMaxJoltage(batteries: List<Int>): Int {
+            val firstMax = batteries.asSequence()
+                .take(batteries.size - 1)
+                .withIndex()
+                .maxBy { it.value }
+            val secondMax = batteries.subList(firstMax.index + 1, batteries.size)
+                .asSequence().max()
+            return firstMax.value * 10 + secondMax
+        }
+    }
+
+    @Nested
+    inner class Task2 {
+        @Test
+        fun testSmall() {
+            val actual = solve(load_test())
+            result.println("Result: $actual")
+            logger.info { "Result: $actual" }
+            assertEquals(5555555, actual)
+        }
+
+        @Test
+        fun testReal() {
+            val actual = solve(load_prod())
+            result.println("Result: $actual")
+            logger.info { "Result: $actual" }
+            assertEquals(55555555, actual)
+        }
+
+        fun solve(txt: String): Any {
+            return 11111111
+        }
+    }
+
+    companion object {
+        val res_prefix = this::class.java.enclosingClass.simpleName.lowercase()
+        fun load_test(): String {
+            return Resources().loadString("${res_prefix}_test.txt")
+        }
+
+        fun load_prod(): String {
+            return Resources().loadString("${res_prefix}_prod.txt")
+        }
+
+        fun parseInput(txt: String): List<List<Int>> {
+            val data = txt.lineSequence().map { line ->
+                line.asSequence().map { it.digitToInt() }.toList()
+            }.toList()
+            return data
+        }
+    }
+}
