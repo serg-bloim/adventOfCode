00036f5824982f90381c7e1b5de4ad7b5bb12150
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-12-07 13:40:17 -0500

    Day2 task1 done bruteforce

--- src/test/kotlin/advent25/Day2.kt
+++ src/test/kotlin/advent25/Day2.kt
@@ -3,8 +3,9 @@ package advent25
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import utils.assertEquals
 import utils.result
-import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class Day2 {
     val logger = KotlinLogging.logger {}
@@ -23,14 +24,41 @@ class Day2 {
         fun testReal() {
             val actual = solve(load_prod())
             result.println("Result: $actual")
-            assertEquals(555555555, actual)
+            assertEquals(55916882972, actual)
             logger.info { "Result: $actual" }
+        }
+
+        @Test
+        fun testIsValid() {
+            assertTrue { isValid(101) }
+            assertTrue { !isValid(1010) }
         }
 
         fun solve(txt: String): Any {
             val data = parseInput(txt)
-            logger.info { data.toList() }
-            return 325354
+            var sum = 0L
+            for (range in data) {
+                val (start, end) = range
+                for (n in start..end) {
+                    if (!isValid(n)) {
+                        sum += n
+                    }
+                }
+            }
+            return sum
+        }
+
+        fun isValid(n: Long): Boolean {
+            val str = n.toString()
+            if (str.length % 2 == 0) {
+                val halfIndex = str.length / 2
+                val first = str.subSequence(0, halfIndex)
+                val second = str.subSequence(halfIndex, str.length)
+                if( first.contentEquals(second)){
+                    return false
+                }
+            }
+            return true
         }
     }
 
@@ -67,10 +95,10 @@ class Day2 {
             return Resources().loadString("${res_prefix}_prod.txt")
         }
 
-        fun parseInput(txt: String): Sequence<Pair<Int, Int>> {
+        fun parseInput(txt: String): Sequence<Pair<Long, Long>> {
             val data = txt.splitToSequence(',')
                 .map { part ->
-                    val (a, b) = part.trim().split('-').map { it.toInt() }
+                    val (a, b) = part.trim().split('-').map { it.toLong() }
                     Pair(a, b)
                 }
             return data
