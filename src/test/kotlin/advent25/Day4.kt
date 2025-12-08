3f584f6956b377d648e11399d8fe928e82845e31
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-12-07 21:40:34 -0500

    upd

--- src/test/kotlin/advent25/Day4.kt
+++ src/test/kotlin/advent25/Day4.kt
@@ -29,9 +29,6 @@ class Day4 {
             logger.info { "Result: $actual" }
         }
 
-        fun IntRange.withoutBorder(n: Int) = IntRange(first + n, last - n)
-        private fun hasRoll(data: List<String>, x: Int, y: Int) = data[y][x] == '@'
-
         fun solve(txt: String): Any {
             val data = addBorder(parseInput(txt), '.')
             var rollsAccessible = 0
@@ -52,13 +49,17 @@ class Day4 {
         }
     }
 
-    private fun addBorder(field: List<String>, ch: Char): List<String> {
+    fun addBorder(field: List<String>, ch: Char): List<String> {
         val borderLine = ch.repeatAsSequence(field[0].length + 2).joinToString("")
         val newField = (sequenceOf(borderLine) +
                 field.asSequence().map { "$ch$it$ch" } +
                 sequenceOf(borderLine)).toList()
         return newField
     }
+
+    fun IntRange.withoutBorder(n: Int) = IntRange(first + n, last - n)
+    fun hasRoll(data: List<String>, x: Int, y: Int) = data[y][x] == '@'
+
 
     @Nested
     inner class Task2 {
@@ -67,7 +68,7 @@ class Day4 {
             val actual = solve(load_test())
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
-            assertEquals(5555555, actual)
+            assertEquals(43, actual)
         }
 
         @Test
@@ -79,7 +80,47 @@ class Day4 {
         }
 
         fun solve(txt: String): Any {
-            return 11111111
+            val inp = addBorder(parseInput(txt), '.')
+            val noRollPenalty = 1000
+            val data = inp.map { IntArray(it.length) { -noRollPenalty } }
+            // For each roll cell bump all its neighbors by +1. This way each cell will have # of rolls around it.
+            // Cells that don't contain rolls themselves have a penalty, so they always expect to be negative.
+            for (y in data.indices.withoutBorder(1)) {
+                for (x in data[0].indices.withoutBorder(1)) {
+                    if (hasRoll(inp, x, y)) {
+                        for (dx in -1..1) {
+                            for (dy in -1..1) {
+                                data[y + dy][x + dx] += 1
+                            }
+                        }
+                        data[y][x] += (noRollPenalty - 1)
+                    }
+                }
+            }
+            val candidates4Removal = mutableSetOf<Pair<Int, Int>>()
+            for (y in data.indices.withoutBorder(1)) {
+                for (x in data[0].indices.withoutBorder(1)) {
+                    val surroundingRolls = data[y][x]
+                    if (surroundingRolls in 0..3) {
+                        candidates4Removal.add(Pair(x, y))
+                    }
+                }
+            }
+            var removedRolls = 0
+            while (candidates4Removal.isNotEmpty()) {
+                val (x, y) = candidates4Removal.first().also { candidates4Removal.remove(it) }
+                data[y][x] = -1
+                removedRolls += 1
+                for (dx in -1..1) {
+                    for (dy in -1..1) {
+                        data[y + dy][x + dx] -= 1
+                        if (data[y + dy][x + dx] == 3) {
+                            candidates4Removal.add(Pair(x, y))
+                        }
+                    }
+                }
+            }
+            return removedRolls
         }
     }
 
