78257779e0b9a7550811eb2028998dd8baaf14cf
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-12-09 22:44:58 -0500

    Day9 task2 real task works

--- src/test/kotlin/advent25/Day9.kt
+++ src/test/kotlin/advent25/Day9.kt
@@ -61,8 +61,11 @@ class Day9 {
             val actual = solve(load_prod())
             result.println("Result: $actual")
             logger.info { "Result: $actual" }
-            assertEquals(55555555, actual)
+            assertEquals(1577956170, actual)
+            // 1577920934 - is not correct
+            // 1570926588 - is not correct
         }
+
         @Test
         fun testReal2() {
             val actual = solve(load_prod())
@@ -72,8 +75,56 @@ class Day9 {
         }
 
         @Test
+        fun drawSVG() {
+            val redTiles = parseInput(load_prod())
+            val points = redTiles.joinToString { "${it.x},${it.y}" }
+            val rectT1 = CoordXY(x = 5698, y = 67727)
+            val rectT2 = CoordXY(x = 94865, y = 50110)
+            val svg = """
+                <svg width="500" height="500" viewBox="0 0 100000 100000" xmlns="http://www.w3.org/2000/svg">
+                  <polygon points="${points}"
+                           fill="orange"
+                           stroke="black"
+                           stroke-width="1" />
+                 <rect
+                   x="${min(rectT1.x, rectT2.x)}"
+                   y="${min(rectT1.y, rectT2.y)}"
+                   width="${abs(rectT1.x - rectT2.x)}"
+                   height="${abs(rectT1.y - rectT2.y)}"
+                   fill="lightblue"
+                   stroke="black"
+                   stroke-width="2"
+                 />
+                 ${
+                redTiles.joinToString("\n") {
+                    """
+                    <circle cx="${it.x}" cy="${it.y}" r="50"
+                                        fill="red"
+                                        stroke="black"
+                                        stroke-width="1"
+                     />
+                """.trimIndent()
+                }
+            }
+                </svg>
+            """.trimIndent()
+            dbg.println(svg)
+            println(svg)
+        }
+
+        @Test
+        fun testCalcArea() {
+//            val t1=CoordXY(5301,67727)
+            val t1 = CoordXY(x = 5301, y = 67727)
+            val t2 = CoordXY(x = 94865, y = 50110)
+            //1577920934
+            //1570926588
+            println(calcArea(t1, t2))
+        }
+
+        @Test
         fun testIsValid() {
-            val redTiles = parseInput(load_test())
+            val redTiles = parseInput(load_prod())
             val leftmostCouple =
                 (redTiles.asSequence() + sequenceOf(redTiles.first())) // account for connection from last to first
                     .zipWithNext() // form connections between two adj. red tiles
@@ -87,16 +138,6 @@ class Day9 {
             val inwardsToRight = leftmostCouple.second.y > leftmostCouple.first.y
             val allSides =
                 redContourStartingLeftmost.repeatForever(2).zipWithNext().take(redContourStartingLeftmost.size)
-            val verticalBoundaries = allSides
-                .filter { (t1, t2) -> t1.x == t2.x }
-                .map { (t1, t2) ->
-                    val from = min(t1.y, t2.y)
-                    val to = max(t1.y, t2.y)
-                    val dir = (t1.y < t2.y) xor !inwardsToRight
-                    Boundary(from, to, t1.x, dir)
-                }
-                .sortedBy { it.offset }
-                .toList()
             val horizontalBoundaries = allSides
                 .filter { (t1, t2) -> t1.y == t2.y }
                 .map { (t1, t2) ->
@@ -107,19 +148,12 @@ class Day9 {
                 }
                 .sortedBy { it.offset }
                 .toList()
-            logger.info { "Horizontal boundaries" }
-            horizontalBoundaries.forEach { b -> logger.info { b } }
-            logger.info { "Vertical boundaries" }
-            verticalBoundaries.forEach { b -> logger.info { b } }
             val rects = listOf(
-                Pair(CoordXY(8, 2), CoordXY(9, 3)),
-                Pair(CoordXY(7, 1), CoordXY(11, 5)),
-                Pair(CoordXY(7, 1), CoordXY(12, 5)),
-                Pair(CoordXY(7, 1), CoordXY(10, 5)),
-                Pair(CoordXY(7, 1), CoordXY(11, 6)),
+                Pair(CoordXY(x = 5301, y = 67727), CoordXY(x = 94865, y = 50110)),
+                Pair(CoordXY(x = 5698, y = 67727), CoordXY(x = 94865, y = 50110)),
             )
             rects.forEach { (t1, t2) ->
-                val valid = isValidRect(t1, t2, verticalBoundaries, horizontalBoundaries)
+                val valid = isValidRect(t1, t2, horizontalBoundaries)
                 logger.info { "Rect $t1 x $t2 is valid: $valid" }
             }
             logger.info { }
@@ -162,7 +196,7 @@ class Day9 {
                 .sortedBy { it.offset }
                 .toList()
             val rl = RateLimiter()
-            val maxArea = redTiles.permutations2()
+            val allRectsSorted = redTiles.permutations2()
 //                .onEach { (t1, t2) ->
 //                    if(t1==CoordXY(9,5) && t2== CoordXY(2,3))
 //                        1+1
@@ -170,11 +204,21 @@ class Day9 {
 //                    val area = calcArea(t1, t2)
 //                    logger.info { "Rect $t1 x $t2 is valid: $valid, area: $area" }
 //                }
+//                .filter { (t1, t2)-> t1 == CoordXY(x=94865, y=50110) || t2 == CoordXY(x=94865, y=50110) }
                 .sortedByDescending { (t1, t2) -> calcArea(t1, t2) }
-                .onEach { (t1, t2) -> rl.onEach(1){ i-> logger.info { "$i Invalid rect: $t1 x $t2, area: ${calcArea(t1, t2)}" } }}
-                .filter { (t1, t2) -> isValidRect(t1, t2, verticalBoundaries, horizontalBoundaries) }
+                .toList()
+            //4758121828
+            val (t1, t2) = allRectsSorted
+                .asSequence()
+                .onEach { (t1, t2) ->
+                    rl.onEach(100) { i ->
+                        logger.info { "$i / (${100 * i / allRectsSorted.size}%) Invalid rect: $t1 x $t2, area: ${calcArea(t1, t2)}, cacheSize: ${greenCache.size}" }
+                    }
+                }
+                .filter { (t1, t2) -> isValidRect(t1, t2, horizontalBoundaries) }
                 .first()
-                .let { (t1, t2) -> calcArea(t1, t2) }
+            val maxArea = calcArea(t1, t2)
+            logger.info { "Max rect: $t1 x $t2 = $maxArea" }
             return maxArea
         }
 
@@ -184,52 +228,49 @@ class Day9 {
     private fun isValidRect(
         t1: CoordXY,
         t2: CoordXY,
-        verticalBoundaries: List<Boundary>,
         horizontalBoundaries: List<Boundary>
     ): Boolean {
         val bottomLeft = CoordXY(min(t1.x, t2.x), min(t1.y, t2.y))
         val topRight = CoordXY(max(t1.x, t2.x), max(t1.y, t2.y))
         val leftSide = generateSequence(bottomLeft.y) { it + 1 }
-            .takeWhile { it <= topRight.y }
+            .takeWhile { it < topRight.y }
             .map { CoordXY(bottomLeft.x, it) }
-        val leftSideValid = leftSide.all { t ->
-
-            hasSupportingBoundary(t.y, t.x, verticalBoundaries)
-        }
-        val rightSide = generateSequence(bottomLeft.y) { it + 1 }
-            .takeWhile { it <= topRight.y }
-            .map { CoordXY(topRight.x, it) }
-        val rightSideValid = rightSide.all { t ->
-            hasSupportingBoundary(t.y, t.x, verticalBoundaries)
-        }
-
-        val bottomSide = generateSequence(bottomLeft.x) { it + 1 }
-            .takeWhile { it <= topRight.x }
-            .map { CoordXY(it, bottomLeft.y) }
-        val bottomSideValid = bottomSide.all { t ->
-            hasSupportingBoundary(t.x, t.y, horizontalBoundaries)
-        }
         val topSide = generateSequence(bottomLeft.x) { it + 1 }
             .takeWhile { it <= topRight.x }
             .map { CoordXY(it, topRight.y) }
-        val topSideValid = topSide.all { t ->
-            hasSupportingBoundary(t.x, t.y, horizontalBoundaries)
+        val rightSide = generateSequence(bottomLeft.y) { it + 1 }
+            .takeWhile { it <= topRight.y }
+            .map { CoordXY(topRight.x, it) }
+        val bottomSide = generateSequence(bottomLeft.x) { it + 1 }
+            .takeWhile { it <= topRight.x }
+            .map { CoordXY(it, bottomLeft.y) }
+        val perimiter = leftSide + topSide + rightSide + bottomSide
+        return perimiter.all { t ->
+//            if(!hasSupportingBoundary(t.x, t.y, horizontalBoundaries))
+//                hasSupportingBoundary(t.x, t.y, horizontalBoundaries)
+            if (greenCache.size > 6000000) {
+                logger.info { "-----Cache restart------" }
+                greenCache.clear()
+            }
+            greenCache.computeIfAbsent(t) { hasSupportingBoundary(t.x, t.y, horizontalBoundaries) }
         }
-        return leftSideValid && rightSideValid && topSideValid && bottomSideValid
     }
+
     val greenCache = mutableMapOf<CoordXY, Boolean>()
     private fun hasSupportingBoundary(coord: Int, offset: Int, boundaries: List<Boundary>): Boolean {
         // We can optimize by doing better search, at least smth like a binary search
         val closestBoundary = boundaries.asSequence()
             .filter { coord in it && it.offset <= offset }
             .maxByOrNull { it.offset }
+//        val closestBoundaryToLeft = boundaries.sea
         if (closestBoundary == null) return false
         if (closestBoundary.offset == offset) return true
+        if (coord == closestBoundary.c1 || coord == closestBoundary.c2) return true
         return closestBoundary.dirTowardsBigger
     }
 
-    private fun calcArea(t1: CoordXY, t2: CoordXY) =
-        abs((t1.x.toLong() - t2.x + 1) * (t1.y - t2.y + 1))
+    private fun calcArea(t1: CoordXY, t2: CoordXY): Long =
+        (abs(t1.x - t2.x) + 1).toLong() * (abs(t1.y - t2.y) + 1)
 
     companion object {
         val res_prefix = this::class.java.enclosingClass.simpleName.lowercase()
